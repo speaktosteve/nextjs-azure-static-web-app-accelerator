40db8ca5434911a53c289a7018db5966397a7c08
Steve Smith <steve.a.smith@avanade.com>
2023-06-05 15:51:30 +0100

    play with build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import 'server-only';
 import { getStars } from './components/common/getStars';
+import { Counter } from './components/common/counter';
 
 const Home = async () => {
   const stars = await getStars();
@@ -8,7 +8,8 @@ const Home = async () => {
     <main className="flex min-h-screen justify-center items-center">
       <section className="text-center">
         <h1 className="text-lg">Next.JS Accelerator</h1>
-        <p className="pt-4">Sample fetch result: {stars.stargazers_count}</p>
+        <p className="py-4">Sample fetch result: {stars.stargazers_count}</p>
+        Client-side counter <Counter />
       </section>
     </main>
   );
