5f4eb6e09961e68ddcf9b829d7e0341899b18ac0
Steve Smith <steve.a.smith@avanade.com>
2023-06-06 17:28:28 +0100

    tidy up routing

--- src/app/components/common/counter.tsx
+++ src/app/components/common/counter.tsx
@@ -8,7 +8,12 @@ export const Counter = () => {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Click me
+      </button>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import { getStars } from './components/common/getStars';
 import { Counter } from './components/common/counter';
 
-const Home = async () => {
+const Page = async () => {
   const stars = await getStars();
 
   return (
-    <main className="flex min-h-screen justify-center items-center">
-      <section className="text-center">
-        <h1 className="text-lg">Next.JS Accelerator</h1>
-        <p className="py-4">Sample fetch result: {stars.stargazers_count}</p>
-        Client-side counter <Counter />
-      </section>
-    </main>
+    <section className="text-center">
+      <h1 className="text-lg">Next.JS Accelerator</h1>
+      <p className="py-4">Sample fetch result: {stars.stargazers_count}</p>
+      Client-side counter <Counter />
+    </section>
   );
 };
 
-export default Home;
+export default Page;
