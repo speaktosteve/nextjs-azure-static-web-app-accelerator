9e507e7e7d97ba1126eae98db98ea4210fe29d54
Steve Smith <steve.a.smith@avanade.com>
2023-06-09 12:17:35 +0100

    trying to add env var

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -2,7 +2,7 @@ const Page = async () => {
   return (
     <section className="text-center">
       <ul>
-        <li>Build version: {process.env.BUILD_VERSION}</li>
+        <li>Release version: {process.env.RELEASE_VERSION}</li>
       </ul>
     </section>
   );
